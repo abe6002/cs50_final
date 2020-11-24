d5aa9c55be3386a51a846ff0871cc3799a95f283
abe6002 <dawsonabraham16@gmail.com>
2020-11-23 19:13:26 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+#Configure Application
+app = Flask(__name__)
+
+#load secret key
+app.config.from_object('config.Config')
+
+
+#Dashboard
+@app.route("/")
+#@login_required - not for testing purposes
+def dashboard():
+    return render_template("layout.html")
+
+
+#Login Screen - #TODO
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    #Forgets old user_id
+    session.clear()
+
+    #if user submits form via POST
+    if request.method == "POST":
+
+        #ensure username was submitted
+        if not request.form.get("user_name"):
+            return None
+
+        #ensure password was submitted
+        elif not request.form.get("password"):
+            return None
+        
+        #query database for username
+        #rows = #TODO
+        
+        #ensures username exists and password matches input
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return None
+
+        #creating a session for the user
+        session["user_id"] = rows[0]["id"]
+
+        #redirect to home page
+        return redirect("/")
+
+    #if user reachs login via GET
+    else:
+        return render_template("login.html")
+
+        
+
+        
---
+++ helpers.py
@@ -0,0 +1,12 @@
+import os
+import requests
+from functools import wraps
+
+#decorates routes to require login
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
